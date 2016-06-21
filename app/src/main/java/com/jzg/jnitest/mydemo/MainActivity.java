7135a96edd860c1de4de7d2342bed38c663a21e0
xiaobao058 <862927084@qq.com>
2016-06-21 17:50:11 +0800

    加入fresco库

--- app/src/main/java/com/jzg/jnitest/mydemo/MainActivity.java
+++ app/src/main/java/com/jzg/jnitest/mydemo/MainActivity.java
@@ -1,13 +1,16 @@
 package com.jzg.jnitest.mydemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fresco.initialize(this);
         setContentView(R.layout.activity_main);
     }
 }
