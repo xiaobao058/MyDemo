2dade04415c06a6aba2bcffc70c67a1690413ec7
xiaobao058 <862927084@qq.com>
2016-06-21 15:58:28 +0800

    Initial commit

---
+++ app/src/main/java/com/jzg/jnitest/mydemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jzg.jnitest.mydemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
